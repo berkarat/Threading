ec9f9d678c7a8009b6d8358876bf873402b4a6f3
Berk Arat <berkarat93@hotmail.com>
2018-10-14 20:33:37 +0300

    Add files via upload

---
+++ ThreadExample/ThreadExample/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ThreadExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing&&(components!=null))
+            {
+                components.Dispose ();
+            }
+            base.Dispose (disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(80, 85);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(203, 45);
+            this.trackBar1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(80, 136);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(192, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(80, 165);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(192, 23);
+            this.progressBar2.TabIndex = 4;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(80, 194);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(192, 23);
+            this.progressBar3.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(328, 136);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 349);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ThreadExample/ThreadExample/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ThreadExample
+{
+    public partial class Form1 : Form
+    {
+        Thread thread_1, thread_2, thread_3, thread_4, thread_5 ;
+
+
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer ();
+
+        public Form1 ()
+        {
+            InitializeComponent ();
+            thread_1=new Thread (new ThreadStart (t_1));
+            thread_2=new Thread (new ThreadStart (t_2));
+            thread_3=new Thread (new ThreadStart (t_3));
+            thread_4=new Thread (new ThreadStart (t_4));
+            thread_5=new Thread (new ThreadStart (t_5));
+
+
+            Control.CheckForIllegalCrossThreadCalls=false;       // THREAD ÇAKIŞMASINI ENGELLER
+            timer.Tick+=new EventHandler (Timer_Tick);
+            timer.Interval=(1000); timer.Start ();
+            timer.Enabled=true;
+
+
+        }
+        string Thread_State;
+        private void button1_Click (object sender, EventArgs e)
+        {
+            
+
+            thread_1.Start (); thread_1.Abort ();
+            Thread_State= thread_1.ThreadState.ToString();
+            Thread_State=thread_1.IsAlive.ToString ();
+            thread_1.Join ();
+            //thread_1.Join (300);   //300 ms kadar işlem yapar
+            Console.WriteLine ("thread_1 bitti");
+            label1.Text="thread_1 bitti";
+            Thread.Sleep (2000);
+            //thread_2.Start ();
+            //thread_2.Join ();
+            label1.Text="thread_2 bitti";
+            Console.WriteLine ("thread_2 bitti");
+            thread_3.Start ();
+            thread_4.Start ();
+        }
+
+      
+
+        private void t_1 ()
+        {
+            thread_5.Start ();
+
+            for (int i = 0; i<=100; i++)
+            {
+                progressBar1.Value=i;
+                Thread.Sleep (20);
+
+            }
+
+
+        }
+        private void t_2 ()
+        {
+            for (int i = 0; i<=100; i++)
+            {
+                //  trackBar1.Value=i;
+                progressBar2.Value=i;
+                Thread.Sleep (20);
+
+            }
+
+        }
+        private void t_3 ()
+        {
+            for (int i = 100; i>=0; i--)
+            {
+                //  trackBar1.Value=i;
+                progressBar3.Value=i;
+                Thread.Sleep (20);
+
+            }
+        }
+        private void t_4 ()
+        {
+
+            for (int i = 0; i<=10; i++)
+            {
+                trackBar1.Value=i;
+
+                Thread.Sleep (200);
+
+            }
+
+
+        }
+        private void t_5 ()
+        {
+
+            for (int i = 0; i<=100; i++)
+            {
+                listBox1.Items.Add (i);
+
+                Thread.Sleep (30);
+
+            }
+
+
+        }
+        private void Timer_Tick (object sender, EventArgs e)
+        {
+            label2.Text=DateTime.Now.ToString ();
+        }
+
+
+    }
+}
